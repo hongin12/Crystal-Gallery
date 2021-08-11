024c4798eab16f9f6ec8a4dab098456651ce6a4d
hongin12 <ghddls0202@gmail.com>
2021-08-11 22:55:31 +0900

    주석 수정한 것

--- CrystalGallery/CrystalGallery/urls.py
+++ CrystalGallery/CrystalGallery/urls.py
@@ -13,7 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-# from CrystalGallery.crystal.views import auction
+# from CrystalGallery.crystal.views import auctionnn
 from django.contrib import admin
 from django.urls import path, include
 from crystal import views
